75bb1469712049f6a12be4846737b77d3044908e
racodin <skkim6.dev@gmail.com>
2019-04-04 12:02:21 +0900

    added Edge browser

--- src/attributes/browser.js
+++ src/attributes/browser.js
@@ -1,18 +1,60 @@
-const LIST = [
-    {
-        name: 'Googlebot',
-        test: [/googlebot/i],
-        match: [/googlebot\/(\d+(\.\d+))/i]
-    }
-]
+var browser = {};
+var nAgt = navigator.userAgent;
+var browserName = navigator.appName;
+var fullVersion = '' + parseFloat(nAgt);
+var majorVersion = parseInt(nAgt, 10);
+var nameOffset, verOffset, ix;
 
-function getFirstMatch(regexp, ua) {
-    const match = ua.match(regexp);
-    return (match && match.length > 0 && match[1]) || '';
+// In Opera, the true version is after "Opera" or after "Version"
+if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 6);
+    if ((verOffset = nAgt.indexOf("Version")) != -1) fullVersion = nAgt.substring(verOffset + 8);
 }
-function getSecondMatch(regexp, ua) {
-    const match = ua.match(regexp);
-    return (match && match.length > 1 && match[2]) || '';
+// In MSIE, the true version is after "MSIE" in userAgent
+else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+    browserName = "Microsoft Internet Explorer";
+    fullVersion = nAgt.substring(verOffset + 5);
+}
+// In Edge, the true version is after "Edge" 
+else if ((verOffset = nAgt.indexOf("Edge")) != -1) {
+    browserName = "Edge";
+    console.log(nAgt);
+    fullVersion = nAgt.substring(verOffset + 5);
+}
+// In Chrome, the true version is after "Chrome" 
+else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+    browserName = "Chrome";
+    fullVersion = nAgt.substring(verOffset + 7);
+}
+// In Safari, the true version is after "Safari" or after "Version" 
+else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+    browserName = "Safari";
+    fullVersion = nAgt.substring(verOffset + 7);
+    if ((verOffset = nAgt.indexOf("Version")) != -1) fullVersion = nAgt.substring(verOffset + 8);
+}
+// In Firefox, the true version is after "Firefox" 
+else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+    browserName = "Firefox";
+    fullVersion = nAgt.substring(verOffset + 8);
+}
+// In most other browsers, "name/version" is at the end of userAgent 
+else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+    browserName = nAgt.substring(nameOffset, verOffset);
+    fullVersion = nAgt.substring(verOffset + 1);
+    if (browserName.toLowerCase() == browserName.toUpperCase()) browserName = navigator.appName;
+}
+// trim the fullVersion string at semicolon/space if present
+if ((ix = fullVersion.indexOf(";")) != -1) fullVersion = fullVersion.substring(0, ix);
+if ((ix = fullVersion.indexOf(" ")) != -1) fullVersion = fullVersion.substring(0, ix);
+
+majorVersion = parseInt('' + fullVersion, 10);
+if (isNaN(majorVersion)) {
+    fullVersion = '' + parseFloat(nAgt);
+    majorVersion = parseInt(nAgt, 10);
 }
 
-getFirstMatch(LIST[0].match, );
+browser.name = browserName;
+browser.version = majorVersion;
+
+export default browser;
